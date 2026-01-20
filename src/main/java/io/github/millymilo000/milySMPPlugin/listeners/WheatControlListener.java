f01070a8c5e089b76c79c4e9dc7506352ab27b0d
Milo <167593567+Millymilo000@users.noreply.github.com>
2026-01-20 16:44:02 -0700

    made it so bonemealing wheat with dispensers doesnt

--- src/main/java/io/github/millymilo000/milySMPPlugin/listeners/WheatControlListener.java
+++ src/main/java/io/github/millymilo000/milySMPPlugin/listeners/WheatControlListener.java
@@ -1,10 +1,13 @@
 package io.github.millymilo000.milySMPPlugin.listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.data.Directional;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockDispenseEvent;
 import org.bukkit.event.block.BlockGrowEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
@@ -16,6 +19,17 @@ public class WheatControlListener implements Listener {
         if (event.hasItem() && event.getItem().getType().equals(Material.BONE_MEAL)) {
             Block block = event.getClickedBlock();
             if (block != null && block.getType().equals(Material.WHEAT)) {
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onDispenser(BlockDispenseEvent event) {
+        if (Material.BONE_MEAL.equals(event.getItem().getType()) && event.getBlock().getBlockData() instanceof Directional direction) {
+            // Find the block its interacting with
+            Material material = event.getBlock().getRelative(direction.getFacing()).getType();
+            if (Material.WHEAT.equals(material)) {
                 event.setCancelled(true);
             }
         }
